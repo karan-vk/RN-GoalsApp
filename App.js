685ca311e2a272ee469dbe43cb45d330d538ea9c
Karan <karanvknarayanan@gmail.com>
2020-10-27 00:25:03 +0530

    goals stored

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
   };
   const addGoalHandler = () => {
-    console.log(enteredGoal);
+    setCourseGoals((prevState) => {
+      [...prevState, enteredGoal];
+    });
   };
   return (
     <View style={styles.screen}>
@@ -21,7 +24,9 @@ export default function App() {
 
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <View>
+        <Text></Text>
+      </View>
     </View>
   );
 }
