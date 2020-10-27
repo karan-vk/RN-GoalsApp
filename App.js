01c42fbc6f0a28d8475aece41b0b082b5830b867
Karan <karanvknarayanan@gmail.com>
2020-10-27 21:15:42 +0530

    scrolling added

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
@@ -24,13 +31,13 @@ export default function App() {
         />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => (
           <View key={goal + Math.random()} style={styles.listItem}>
             <Text>{goal}</Text>
           </View>
         ))}
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -56,5 +63,6 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     margin: 10,
+    marginVertical: 10,
   },
 });
